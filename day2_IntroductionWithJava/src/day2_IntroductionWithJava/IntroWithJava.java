ddeaa9267b6bb372f06be34a9fc9540a1620335b
vikram7102 <bhosalevikram7102@gmail.com>
2022-09-26 19:12:10 +0530

    day2_intro with java

---
+++ day2_IntroductionWithJava/src/day2_IntroductionWithJava/IntroWithJava.java
@@ -0,0 +1,11 @@
+package day2_IntroductionWithJava;
+
+public class IntroWithJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=12;
+		System.out.println(x);
+	}
+
+}
