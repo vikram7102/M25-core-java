cbc97dd616997dd1270f07756690d5e7c145d934
vikram7102 <bhosalevikram7102@gmail.com>
2022-10-12 23:38:01 +0530

    Done With day 6

---
+++ day6_abstract,final,super_keyword/src/org/tnsindia/finalkeyword/DemoOnFinalClass.java
@@ -0,0 +1,28 @@
+package org.tnsindia.finalkeyword;
+class FinalClass
+{
+	protected String name="Jenny";
+}
+//final class cannot be inherited
+/*final class FinalClass
+{
+	protected String name="Jenny";
+	
+}*/
+//child class(Inheritance)
+class ChildFinal extends FinalClass
+{
+	public void print()
+	{
+		System.out.println(name);
+	}
+}
+public class DemoOnFinalClass {
+
+	public static void main(String[] args) {
+		ChildFinal c =new ChildFinal () ;
+		c.print();
+
+	}
+
+}
