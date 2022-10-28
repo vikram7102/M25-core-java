a1ada6ca68112b7c2b034ad3ab7bcbf1a43522a0
vikram7102 <bhosalevikram7102@gmail.com>
2022-10-28 16:36:45 +0530

    done with day14

---
+++ day14_stream_api/src/org/tnsindia/Streamdemo/FilterExample.java
@@ -0,0 +1,14 @@
+package org.tnsindia.Streamdemo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FilterExample {
+
+	public static void main(String[] args) {
+		List<String>obj=Arrays.asList(new String[]{"Nayna","xy","Sushma","Bhakti"});
+		obj.stream().filter((i->i.length()>4)).forEach((i)->System.out.print(i+" "));
+
+	}
+
+}
---
+++ day14_stream_api/src/org/tnsindia/Streamdemo/MappingDemo.java
@@ -0,0 +1,14 @@
+package org.tnsindia.Streamdemo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MappingDemo {
+
+	public static void main(String[] args) {
+		List<String>obj=Arrays.asList(new String[]{"Nayna","xy","Sushma","Bhakti"});
+		obj.stream().map(i->i.length()).forEach((i)->System.out.print(i+" "));
+
+	}
+
+}
