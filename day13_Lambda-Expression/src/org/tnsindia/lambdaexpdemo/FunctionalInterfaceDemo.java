4d5da3ba44373b3bbff1a060b09565804084d7d5
vikram7102 <bhosalevikram7102@gmail.com>
2022-10-28 16:35:39 +0530

    done with day 13

---
+++ day13_Lambda-Expression/src/org/tnsindia/lambdaexpdemo/FunctionalInterfaceDemo.java
@@ -0,0 +1,7 @@
+package org.tnsindia.lambdaexpdemo;
+@FunctionalInterface
+public interface FunctionalInterfaceDemo {
+	//1.Functional interface contains exactly one abstract method
+		//abstract method
+		String display(String str);
+}
