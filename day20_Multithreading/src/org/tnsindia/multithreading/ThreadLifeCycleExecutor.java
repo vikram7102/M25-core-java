8ab8c8e69bcf7b0b54f611d3050eee4d9a422ba9
vikram7102 <bhosalevikram7102@gmail.com>
2022-11-06 16:38:50 +0530

    done with the multithreading concepts

---
+++ day20_Multithreading/src/org/tnsindia/multithreading/ThreadLifeCycleExecutor.java
@@ -0,0 +1,12 @@
+package org.tnsindia.multithreading;
+
+
+public class ThreadLifeCycleExecutor {
+
+	public static void main(String[] args) {
+		ThreadLifeCycleDemo d=new ThreadLifeCycleDemo();
+		d.start();
+
+	}
+
+}
