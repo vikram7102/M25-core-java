7311ec7b580cc2c5fe862d2a506b983859f105ae
vikram7102 <bhosalevikram7102@gmail.com>
2022-11-06 17:37:40 +0530

    unit testing

---
+++ day18_Junit5/test/org/tnsindia/junit5demo/AssumptionDemo.java
@@ -0,0 +1,24 @@
+package org.tnsindia.junit5demo;
+
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+
+class AssumptionDemo {
+
+	@Test
+	void display()
+	{
+		System.setProperty("Shiwani", "Kum");
+		//will evaluate the given assumption and if it results in true then the given test is allowed to execute 
+		Assumptions.assumeTrue("Kum".equals(System.getProperty("Shiwani")));
+	}
+	@Test
+	void display1()
+	{
+		System.setProperty("Shiwani", "Kum");
+		//Will evaluate the given assumption and test will run if result is false.
+		Assumptions.assumeFalse("Kumbh".equals(System.getProperty("Shiwani")));
+	}
+	
+
+}
